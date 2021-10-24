61a98389c335bd3a0d549e338f1587a10eea9627
Manish Panda <pandamanish49@gmail.com>
2021-10-25 04:21:05 +0530

    wwks

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+let nav = document.getElementById("nav");
+var media = window.matchMedia("max-width:730px");
+
+function menubar()
+{
+    nav.style.display="block";
+}
+function closebar()
+{
+    nav.style.display="none";
+}
+
